c5c1b435d0868f86b9c93173c20c8d956f9b6f1c
Alex <shaftoe@users.noreply.github.com>
2020-02-13 19:28:17 +0300

    fix: Add setup.py metadata

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ VERSION = None
 with open("VERSION") as f:
     VERSION = f.read().rstrip()
 
+with open("README.md") as f:
+    LONG_DESC = f.read()
 
 setuptools.setup(
     name="api-l3x-in",
     version=VERSION,
 
     description="api.l3x.in source code",
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
 
     author="Alexander Fortin",
 
@@ -26,8 +30,10 @@ setuptools.setup(
         "aws-cdk.aws-logs",
         "aws-cdk.aws-sns",
         "aws-cdk.aws-sns-subscriptions",
+        "boto3",
     ],
 
     python_requires=">=3.8",
 
+    # FIXME: add proper metadata
 )
