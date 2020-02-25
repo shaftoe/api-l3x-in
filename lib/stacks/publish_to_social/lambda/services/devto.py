cd6324f71baf3f3919845d44d68c5bb06d7a7d31
Alex <shaftoe@users.noreply.github.com>
2020-02-25 11:10:49 +0300

    fix(devto): inject original blog reference to markdown content

--- lib/stacks/publish_to_social/lambda/services/devto.py
+++ lib/stacks/publish_to_social/lambda/services/devto.py
@@ -1,5 +1,6 @@
 from os import environ as env
 import json
+from typing import Iterable
 import urllib.parse
 
 import utils
@@ -11,22 +12,49 @@ DEVTO_URL = "https://dev.to/"
 DEVTO_PUBLISH_URL = urllib.parse.urljoin(DEVTO_URL, "api/articles")
 
 
+def get_content_from_gh(url: str) -> Iterable[str, str]:
+    """Fetch blog content from GitHub and inject original blog link reference."""
+    raw = helpers.get_file_from_github(helpers.from_link_to_jekyll_md(url))
+
+    index = raw.index("---\n", 1)
+    front_matter = raw[4:index].strip()
+    article = raw[index + 4:].strip()
+
+    return (front_matter, article)
+
+
 def post_status(content: utils.LambdaEvent) -> str:
     """Post content to dev.to api/articles endpoint
 
     ref: https://docs.dev.to/api/#operation/createArticle
     """
+    original_ref = env["DEVTO_ORIGINAL_REF"]
+    front_matter, article = get_content_from_gh(content["url"])
+
+    if len(content["categories"]) > 4:
+        utils.Log.warning("Found more than 4 categories, removing following: %s",
+                          content["categories"][4:])
+        content["categories"] = content["categories"][:4]
+
+    body = f"""---
+{front_matter}
+canonical_url: {content["url"]}
+published: true
+tags: {" ,".join(content["categories"])},
+---
+
+{original_ref}
+
+{article}"""
 
     data = {
         "article": {
             "title": content["title"],
             "description": content["description"],
-            "body_markdown":
-                helpers.get_file_from_github(
-                    helpers.from_link_to_jekyll_md(content["url"])),
+            "body_markdown": body,
+            "published": True,
+            "canonical_url": content["url"],
         },
-        "published": True,
-        "canonical_url": content["url"],
     }
 
     return helpers.send_http_request(
