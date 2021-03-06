c103922c6ae3bf1e860d69296d29813a8bc1f1e2
Alex <shaftoe@users.noreply.github.com>
2021-03-06 16:57:02 +0100

    Disable pagespeed stack

--- lib/cdk.py
+++ lib/cdk.py
@@ -16,9 +16,7 @@ from stacks.api import ApiStack
 from stacks.backups_monitor import BackupsMonitorStack
 from stacks.deployer import DeployerStack
 from stacks.log_report import LogReportStack
-from stacks.mongodumper import MongodumperStack
 from stacks.notifications import NotificationsStack
-from stacks.pagespeed import PageSpeedStack
 from stacks.pocket_to_kindle import PocketToKindleStack
 from stacks.publish_to_social import SocialPublishStack
 from stacks.whois_poller import WhoisStack
@@ -50,21 +48,11 @@ PUBLISH_TO_SOCIAL_STACK = SocialPublishStack(
     },
 )
 
-PAGESPEED_STACK = PageSpeedStack(
-    APP,
-    'pagespeed',
-    tags={
-        'Managed': 'cdk',
-        'Name': 'pagespeed',
-    },
-)
-
 ApiStack(
     APP,
     'api',
     lambda_notifications=NOTIFICATIONS_STACK.pushover,
     social_log_group=PUBLISH_TO_SOCIAL_STACK.log_group,
-    pagespeed_table=PAGESPEED_STACK.table,
     tags={
         'Managed': 'cdk',
         'Name': 'api',
--- lib/stacks/api/api/main.py
+++ lib/stacks/api/api/main.py
@@ -60,40 +60,12 @@ Desc: {description}
         }).text
 
 
-def pagespeed_report(_: utils.LambdaEvent) -> List[Dict]:
-    """Return report from Google Pagespeed data stored in DynamoDB."""
-    data = aws.scan_dynamodb_table(env["PAGESPEED_TABLE"])
-
-    if data["Count"] == 0:
-        raise utils.HandledError(message="Unexpected DynamoDB response: empty table",
-                                 status_code=500)
-
-    items = [{"url": item['url']['S'],
-              "latest_score_value": float(item['latest_score_value']['N']),
-              "latest_score_timestamp": item['latest_score_timestamp']['S']}
-             for item in data["Items"]]
-
-    utils.Log.debug("Items: %s", items)
-
-    errors = False
-    for item in items:
-        if not 0.95 < item["latest_score_value"] <= 1:
-            item["error"] = True
-            errors = True
-
-    if errors:
-        raise utils.HandledError(message=items, status_code=400)
-
-    return items
-
-
 def handler(event, context) -> utils.Response:
     """Lambda entry point.
 
     Public HTTPS REST API entry point
     """
     router_map = {
-        "GET /pagespeed_report": pagespeed_report,
         "GET /robots.txt": lambda _: "User-agent: *\nDisallow: /",
         "GET /social_report": social_report,
         "POST /contact": contact,
